d9335d391c17f2e89957b0dfbf9fdbc4a387230b
nicor88 <6278547+nicor88@users.noreply.github.com>
2021-04-28 13:13:12 +0200

    fix bad reference schema

--- snowflake/external_function.sql
+++ snowflake/external_function.sql
@@ -2,7 +2,7 @@ use role sysadmin;
 
 -- the role that you use need to have access to the api integration
 
-create or replace external function heycar_helper.my_external_function(input variant)
+create or replace external function my_schema.my_external_function(input variant)
   returns variant
   api_integration = api_integration
   as 'api_gateway_endpoint/stage/resource';
